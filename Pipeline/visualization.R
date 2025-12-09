7f585ec42552291faa142bb11cb216d3ee0344d9
hienbui1001 <hien_bui@brown.edu>
2025-12-09 08:09:52 -0500

    x

--- Pipeline/visualization.R
+++ Pipeline/visualization.R
@@ -39,17 +39,72 @@ day_graph <- function(optimal){
 }
 
 #Route visualization function: This function will allow us to view what routes
-#struggle across all days
-route_graph <- function(optimal){
+#struggle across all days.
+route_graphs <- function(optimal){
   
-  #format the data for specific days
+  #format the data for specific routes
   optimal_route <- optimal %>%
     group_by(Route) %>%
-    summarize(optimize_count_total = sum(optimize_count))
+    summarize(optimize_count = sum(optimize_count)) %>%
+    mutate(Route = factor(Route))
+  
+  #Split table into positive and negative
+  few_direct_routes <- optimal_route %>%
+    filter(optimize_count > 0)
+  
+  not_connected <- optimal_route %>%
+    filter(optimize_count < 0)
+  
+  #plot stations that need more direct routes
+  few <- ggplot(few_direct_routes)+
+    geom_col(aes(x = Route,
+                   y = optimize_count),
+             color = "darkgreen",
+             fill = "lightgreen")
+  
+  #plot stations that need better connections
+  conn <- ggplot(not_connected)+
+    geom_col(aes(x = Route,
+                 y = optimize_count),
+             color = "red",
+             fill = "pink")
+print(few)
+print(conn)
 }
 
 #Day paired with Route Visualization Function: This function will allow us to
-#view specifically what routes struggle on what days
-dr_graph <- function(optimal){
+#view specifically what routes struggle on what days using a geom_tile. This will
+#only give us a general overview.
+dxr_graph <- function(optimal){
+  
+  #format data so that it can be made into heat map
+  dxr_table <- optimal %>%
+    mutate(Day.of.Week = factor(Day.of.Week,
+                                levels = 1:7,
+                                labels = c("Mon","Tue","Wed","Thu","Fri","Sat","Sun")),
+           Route = factor(Route))
+  
+  #Plot the data
+  ggplot(dxr_table,
+         aes(x = Route,
+             y = Day.of.Week,
+             fill = optimize_count)) +
+           geom_tile()+
+           scale_fill_gradient2(low = "red",
+                                mid = "white",
+                                high = "blue")
   
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ pipeline.R
@@ -0,0 +1,5 @@
+#This script runs everything in order to reach the final analysis
+source("Pipeline/clean_data.R")
+source("Pipeline/metrics.R")
+source("Pipeline/optimal_range.R")
+source("Pipeline/visualization.R")
