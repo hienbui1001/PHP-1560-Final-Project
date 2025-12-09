dffa826fc655918fe8f9307d3b4b45a0c45fcab3
shravesies <shravya_sunkugari@brown.edu>
2025-12-09 11:40:26 -0500

    Update clean_dates.R

--- Connectivity Pipeline/clean_dates.R
+++ Connectivity Pipeline/clean_dates.R
@@ -1,3 +1,11 @@
+#' Cleaning Dates for Connectivity
+#' 
+#' @description This function is meant to clean the raw data to put the 
+#' Times in POSIXct form and clear out unneccesary info
+#' @param1 raw_data a data frame that contains all the simulated ripta data
+#' that is located in the main folder of the GitHub repository
+#' @return This function returns a data frame with Trip, Route, Stop Number, 
+#' and POSIXct Date_Time
 
 source("~/GitHub/PHP-1560-Final-Project/Pipeline/clean_data.R")
 
